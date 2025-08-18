788b5dc3b95fc17d139edf13297ada172b28b737
Anurag Singh <anurags031200@gmail.com>
2025-08-18 09:04:48 -0700

    Add widget and math packages with configuration and components
    - Created a new widget application with Next.js, including configuration files and a basic layout.
    - Added a math package with add and subtract functions.
    - Updated pnpm-lock.yaml to include new dependencies for the widget and math packages.
    - Configured ESLint and TypeScript for the new packages.
    - Implemented a simple UI in the widget app using components from the UI package.
    - Set up Tailwind CSS and theming support in the widget app.

---
+++ packages/math/src/subtract.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.subtract = void 0;
+var subtract = function (a, b) { return a - b; };
+exports.subtract = subtract;
